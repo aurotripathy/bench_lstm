d2ed7fee371d6b4870a7abee0df426c172151683
Auro Tripathy <ipserv@yahoo.com>
2019-05-02 21:21:15 +0000

    initial

---
+++ utils.py
@@ -0,0 +1,80 @@
+"""
+Utilities that suppport the benchmark
+"""
+import sys
+import numpy as np
+
+
+def set_hyperparams():
+    """ lSTM hyperparameters"""
+    lstm_size = 320
+    learning_rate = 1e-3
+    seq_len = 100
+    batch_size = 64
+    batches = 500
+    return lstm_size, learning_rate, seq_len, batch_size, batches
+
+
+def get_batch(seed=11, shape=(64, 100, 125), classes=10):
+    """
+    Generate random data and labels
+    The input has shape time, batch, input dimension
+    """
+    batch_size, _, _ = shape
+    np.random.seed(seed)
+
+    # Samples, time, batch, X
+    t_b_X = np.float32(np.random.uniform(-1, 1, (shape)))
+
+    # Targets, single output per batch
+    b_y = np.int32(np.random.randint(low=0, high=classes - 1, size=batch_size))
+
+    return t_b_X, b_y
+
+
+def print_results(batch_loss_list, batch_time_list, train_start, train_end):
+    """
+    Validate amd print the results by reviewing
+    the loss behaviour at every batch
+    """
+    abort = False
+
+    # 0. Check if loss is numeric (not NAN and not inf)
+    check_loss = [np.isfinite(loss) for loss in batch_loss_list]
+    if False not in check_loss:
+        print('>>> Loss check 1/2 passed: loss is finite {}'.format(np.unique(check_loss)))
+    else:
+        print(
+            '!!! Loss check 1/2 failed: loss is NOT finite {}'.format(np.unique(check_loss)))
+        abort = True
+
+    # 1. Check if loss is decreasing
+    check_loss = np.diff(batch_loss_list)
+    if np.sum(check_loss) < 0:
+        print('>>> Loss check 2/2 passed: loss is globally decreasing')
+    else:
+        print('!!! Loss check 2/2 failed: loss is NOT globally decreasing')
+        abort = True
+
+    # 2. Check deviation between the full loop time and the sum of individual batches
+    loop_time = train_end - train_start
+    batch_time_sum = np.sum(batch_time_list)
+    factor = loop_time / batch_time_sum
+    deviation = np.abs((1 - factor) * 100)
+
+    if deviation < 1:  # Less than 1% deviation
+        print(
+	    '>>> Timing check passed -  < 1% deviation between loop time and sum of batches')
+
+        print("Total time {:.3f} seconds. Sum of batch times {:.3f} seconds. Deviation [%] {:.3f}".format(loop_time,
+	                                                                                                  batch_time_sum,
+													  deviation))
+    else:
+        print('!!! Timing check failed - Deviation > 1% ::: Loop time {:.3f} ::: Sum of batch times {:.3f} :::'
+              ' Deviation [%] {:.3f}'.format(loop_time, batch_time_sum, deviation))
+        abort = True
+
+    if abort:
+        sys.exit('!!! Abort benchmark.')
+        print('=' * 100)
+	
